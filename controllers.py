24640f58878d270c62af75c7d1430dea27f44ed8
Claudio Davi <cdavisouza@gmail.com>
2017-10-22 15:45:29 -0200

    front end e refatoracao do controller

--- controllers.py
+++ controllers.py
@@ -4,6 +4,24 @@ from app.emails import send_email
 def create_user(user_name, user_email):
     return Dev(user_name, user_email)
 
+def handle_form(form):
+    dev = create_user(form.name.data, form.email.data)
+    front = is_front(form.skill_html.data,
+                form.skill_css.data,
+                form.skill_js.data
+                )
+    back = is_back(form.skill_python.data,
+                form.skill_django.data
+                )
+    mobile = is_mobile(form.skill_ios.data,
+                form.skill_android.data
+                )
+    dev.positions = {'Front End': front,
+                     'Back End': back,
+                     'Mobile': mobile
+                     }
+    handle_email(dev)
+
 def is_front(html,css,js):
     if html >= 7 and js >=7 and css >= 7:
         return True
--- views.py
+++ views.py
@@ -10,36 +10,18 @@ def index():
 
     form = DevForm()
 
-    if request.method == 'POST' and form.is_submitted():
+    if request.method == 'POST' and form.is_submitted() and form.validate_on_submit():
 
-        dev = ctrl.create_user(form.name.data, form.email.data)
+        ctrl.handle_form(form)
 
-        is_front = ctrl.is_front(form.skill_html.data,
-                                 form.skill_css.data,
-                                 form.skill_js.data
-                                 )
-
-        is_back = ctrl.is_back(form.skill_python.data,
-                               form.skill_django.data
-                               )
-
-        is_mobile = ctrl.is_mobile(form.skill_ios.data,
-                                   form.skill_android.data
-                                   )
-
-        dev.positions = {'Front End': is_front,
-                         'Back End': is_back,
-                         'Mobile': is_mobile
-                         }
-
-        ctrl.handle_email(dev)
         return redirect('/thanks')
 
     return render_template('index.html',
-                           title='HOME',
-                           form = form,
+                           title='Inicio',
+                           form=form,
                            )
+
 
 @app.route("/thanks")
 def thanks():
-    return render_template('thanks.html', title='THANKS')
+    return render_template('thanks.html', title='Obrigado')
